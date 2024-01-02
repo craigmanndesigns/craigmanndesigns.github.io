44a6feca6958f1aefa1942defabfff844d4389b2
Craig Mann <craig.mann@truelayer.com>
2024-01-02 14:16:24 +0000

    Updates

---
+++ component---src-pages-storyblok-entry-full-slug-js-866e0458c63d5078fc83.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbrutalism_cmd=self.webpackChunkbrutalism_cmd||[]).push([[238],{2724:function(e,t,n){n.r(t);var a=n(7294),c=n(6010),l=n(477),s=n(7434);t.default=e=>{let{data:t}=e,n=t.storyblokEntry;n=(0,l.f0)(n);const r=n.content.body.map((e=>a.createElement(l.Xj,{blok:e,key:e._uid})));return a.createElement(s.Z,null,a.createElement("div",Object.assign({},(0,l.LF)(n.content),{className:(0,c.Z)("cont")}),r))}}}]);
+//# sourceMappingURL=component---src-pages-storyblok-entry-full-slug-js-866e0458c63d5078fc83.js.map
