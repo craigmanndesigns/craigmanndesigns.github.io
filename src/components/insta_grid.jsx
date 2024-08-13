2794f715cf356d9634a1c760a77a24e92d9ce72a
Craig Mann <craig.mann@truelayer.com>
2024-08-13 15:11:41 +0100

    insta update

--- src/components/insta_grid.jsx
+++ src/components/insta_grid.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
-import { storyblokEditable, StoryblokComponent } from "gatsby-source-storyblok";
+import React from "react";
 import clsx from "clsx";
 
 const InstaGrid = ({ items }) => {
